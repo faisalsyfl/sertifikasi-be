32e8e5b4dd736672deaa20823fdd5d465fa90232
kowlon <wahyuanggana1@gmail.com>
2020-12-02 20:52:21 +0700

    feat: adding dingo handle exception

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+use App\Traits\RestApi;
 
 class AppServiceProvider extends ServiceProvider
 {
+    use RestApi;
     /**
      * Bootstrap any application services.
      *
@@ -13,7 +21,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        app('Dingo\Api\Exception\Handler')->register(function (UnauthorizedHttpException $exception) {
+            return $this->errorRequest(401, $exception->getMessage());
+        });
+
+        app('Dingo\Api\Exception\Handler')->register(function (TokenBlacklistedException $exception) {
+            return $this->errorRequest(401, $exception->getMessage());
+        });
+
+        app('Dingo\Api\Exception\Handler')->register(function (TokenExpiredException $exception) {
+            return $this->errorRequest(401, $exception->getMessage());
+        });
+
+        app('Dingo\Api\Exception\Handler')->register(function (TokenInvalidException $exception) {
+            return $this->errorRequest(401, $exception->getMessage());
+        });
+
+        app('Dingo\Api\Exception\Handler')->register(function (MethodNotAllowedHttpException $exception) {
+            return $this->errorRequest(405, $exception->getMessage());
+        });
     }
 
     /**
