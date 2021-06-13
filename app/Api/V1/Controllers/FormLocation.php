d91330d52c681c25aa94386751decd647fc283a8
wahyu anggana <wahyuanggana1@gmail.com>
2021-06-13 07:31:50 +0700

    fix location

--- app/Api/V1/Controllers/FormLocation.php
+++ app/Api/V1/Controllers/FormLocation.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Api\V1\Controllers\Form;
+namespace App\Api\V1\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
--- routes/api.php
+++ routes/api.php
@@ -88,10 +88,10 @@ $api->version('v1', function ($api) { // Always keep this to v1, and ignore acce
             $api->get('/', 'App\\Api\\V1\\Controllers\\MasterLocation@showCity');
         });
         $api->group(['middleware' => 'jwt.auth', 'prefix' => 'form'], function (Router $api) {
-            $api->get('location/', 'App\\Api\\V1\\Controllers\\Form\\FormLocation@index');
-            $api->get('location/{id}', 'App\\Api\\V1\\Controllers\\Form\\FormLocation@index');
-            $api->post('location', 'App\\Api\\V1\\Controllers\\Form\\FormLocation@storeFormLocation');
-            $api->delete('location/{id}', 'App\\Api\\V1\\Controllers\\Form\\FormLocation@destroy');
+            $api->get('location/', 'App\\Api\\V1\\Controllers\\FormLocation@index');
+            $api->get('location/{id}', 'App\\Api\\V1\\Controllers\\FormLocation@index');
+            $api->post('location', 'App\\Api\\V1\\Controllers\\FormLocation@storeFormLocation');
+            $api->delete('location/{id}', 'App\\Api\\V1\\Controllers\\FormLocation@destroy');
         });
 
         #protect with Jwt Auth
