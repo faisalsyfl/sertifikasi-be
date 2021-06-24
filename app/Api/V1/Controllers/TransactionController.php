88c17e89342fcb2fdbe2137a104cd0d90afc5f40
wahyu anggana <wahyuanggana1@gmail.com>
2021-06-24 09:52:57 +0700

    fix, swagger, routes

--- app/Api/V1/Controllers/TransactionController.php
+++ app/Api/V1/Controllers/TransactionController.php
@@ -77,6 +77,7 @@ class TransactionController extends Controller
      *  security={{ "apiAuth": {} }}
      * )
      */
+
     /**
      * @OA\Get(
      *  path="/api/v1/qsc/{id}",
@@ -139,7 +140,6 @@ class TransactionController extends Controller
      *      @OA\Schema(
      *           type="string",
      *           default="QSC1"
-
      *      )
      *   ),
      *  @OA\Parameter(
@@ -158,6 +158,67 @@ class TransactionController extends Controller
      *           type="string",
      *      )
      *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Success"
+     *  ),
+     *  @OA\Response(response=201,description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *  @OA\Response(response=401,description="Unauthenticated"),
+     *  @OA\Response(response=400,description="Bad Request"),
+     *  @OA\Response(response=404,description="not found"),
+     *  @OA\Response(response=403,description="Forbidden"),
+     *  security={{ "apiAuth": {} }}
+     * )
+     */
+
+    /**
+     * @OA\Post(
+     *  path="/api/v1/qsc2/store",
+     *  summary="Save and Edit - Step 2",
+     *  tags={"Form"},     * @OA\RequestBody(
+     * @OA\JsonContent(
+     *   type="object",
+     *   @OA\Property(property="manajemen_mutu", type="boolean"),
+     *   @OA\Property(property="manajemen_lingkungan", type="boolean"),
+     *   @OA\Property(property="manajemen_keselamatan", type="boolean"),
+     *   @OA\Property(property="industri_hijau", type="boolean"),
+     *   @OA\Property(property="status_aplikasi_sertifikasi", type="string"),
+     *   @OA\Property(property="auditi_id", type="integer"),
+     *   @OA\Property(property="jumlah_personil_management", type="integer"),
+     *   @OA\Property(property="jumlah_personil_administrasi", type="integer"),
+     *   @OA\Property(property="jumlah_personil_part_time", type="integer"),
+     *   @OA\Property(property="jumlah_personil_non_permanen", type="string"),
+     *   @OA\Property(property="jumlah_personil_shift_1", type="integer"),
+     *   @OA\Property(property="jumlah_personil_shift_2", type="integer"),
+     *   @OA\Property(property="jumlah_personil_shift_3", type="integer"),
+     *   @OA\Property(property="jumlah_personil_efektif", type="integer"),
+     *   @OA\Property(property="alamat_id", type="integer"),
+     *   @OA\Property(property="ruang_lingkup", type="string"),
+     *   @OA\Property(property="informasi_tambahan_1", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_2", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_3", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_4", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_5", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_6", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_7", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_8", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_9", type="boolean"),
+     *   @OA\Property(property="informasi_tambahan_1_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_2_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_3_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_4_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_5_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_6_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_7_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_8_keterangan", type="string"),
+     *   @OA\Property(property="informasi_tambahan_9_keterangan", type="string"),
+     *   @OA\Property(property="tanda_tangan_formulir", type="integer"),
+     * )
+     * ),
      *   @OA\Response(
      *     response=200,
      *     description="Success"
@@ -251,13 +312,54 @@ class TransactionController extends Controller
      *  security={{ "apiAuth": {} }}
      * )
      */
+
+    /**
+     * @OA\Get(
+     *  path="/api/v1/qsc2/list/{id}",
+     *  summary="Detail - Step 2",
+     *  tags={"Form"},
+     *  @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      required=false,
+     *      description="Fill with id_transaction",
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *  @OA\Parameter(
+     *      name="mode",
+     *      in="query",
+     *      required=false,
+     *      @OA\Schema(
+     *           type="string",
+     *           default="QSC1"
+     *      )
+     *   ),
+     *  @OA\Response(response=200,description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *  @OA\Response(response=201,description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *  @OA\Response(response=401,description="Unauthenticated"),
+     *  @OA\Response(response=400,description="Bad Request"),
+     *  @OA\Response(response=404,description="not found"),
+     *  @OA\Response(response=403,description="Forbidden"),
+     *  security={{ "apiAuth": {} }}
+     * )
+     */
     public function list(Request $request, $id)
     {
         switch ($request->mode) {
             case 'QSC1':
                 $res = $this->Qsc1->list($request, $id);
                 if (!$res["status"])
-                    return $this->errorRequest(422, 'Validation Error', $res["error"]);
+                    return $this->errorRequest(422, 'Validation Error', $res["data"]);
 
                 $transaction = Transaction::where('id', $id)->first();
                 $final = array_merge($transaction->toArray(), ['form' => $this->serializeForm($res['data'])]);
@@ -268,6 +370,7 @@ class TransactionController extends Controller
                 $res = $this->Qsc2->list($request, $id);
                 if (!$res["status"])
                     return $this->errorRequest(422, 'Validation Error', $res["data"]);
+
                 $transaction = Transaction::where('id', $id)->first();
                 $final = array_merge($transaction->toArray(), ['form' => $this->serializeForm($res['data'])]);
                 return $this->output($final);
