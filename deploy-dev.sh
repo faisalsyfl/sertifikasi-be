ffd7efed9e5c40a4422007e4170bcbcb37c493c2
wahyu anggana <wahyuanggana1@gmail.com>
2021-08-08 23:08:33 +0700

    new deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,13 +4,14 @@ echo "Deploying on docker DEV"
 git reset --hard develop
 git fetch
 git pull origin develop
-docker-compose up --build -d
-docker-compose run --rm artisan migrate
-docker-compose run --rm artisan cache:clear
-docker-compose run --rm artisan config:clear
-docker-compose run --rm artisan config:cache
-docker-compose run --rm composer install
-docker-compose run --rm composer dumpautoload
-docker-compose run --rm artisan l5-swagger:generate
+
+docker-compose -f docker-compose-dev.yml up --build -d
+docker-compose -f docker-compose-dev.yml run --rm artisan migrate
+docker-compose -f docker-compose-dev.yml run --rm artisan cache:clear
+docker-compose -f docker-compose-dev.yml run --rm artisan config:clear
+docker-compose -f docker-compose-dev.yml run --rm artisan config:cache
+docker-compose -f docker-compose-dev.yml run --rm composer install
+docker-compose -f docker-compose-dev.yml run --rm composer dumpautoload
+docker-compose -f docker-compose-dev.yml run --rm artisan l5-swagger:generate
 
 echo "application deployed DEV"
