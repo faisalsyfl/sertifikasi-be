ec8879085aff3795b1ec6cee2b95cbee57762959
wahyu anggana <wahyuanggana1@gmail.com>
2021-08-08 22:46:49 +0700

    build every push dev

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,7 +4,7 @@ echo "Deploying on docker DEV"
 git reset --hard develop
 git fetch
 git pull origin develop
-
+docker-compose up --build -d
 docker-compose run --rm artisan migrate
 docker-compose run --rm artisan cache:clear
 docker-compose run --rm artisan config:clear
