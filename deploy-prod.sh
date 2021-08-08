1f1faaf2564910b975ae62c8b1c38eaadc7f97d3
wahyu anggana <wahyuanggana1@gmail.com>
2021-08-08 23:04:02 +0700

    force

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,7 +6,7 @@ git fetch
 git pull origin master
 
 docker-compose up --build -d
-docker-compose run --rm api_sifion_artisan_prod migrate
+docker-compose run --rm api_sifion_artisan_prod migrate --force
 docker-compose run --rm api_sifion_artisan_prod cache:clear
 docker-compose run --rm api_sifion_artisan_prod config:clear
 docker-compose run --rm api_sifion_artisan_prod config:cache
