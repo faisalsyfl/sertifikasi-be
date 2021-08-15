6c20082ff7cc4494d113d845368749a7d6ca57d6
kowlon <wahyuanggana1@gmail.com>
2021-08-15 10:48:31 +0700

    deplo prod fix

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,13 +5,16 @@ git reset --hard master
 git fetch
 git pull origin master
 
-docker-compose -f docker-compose-prod.yml up --build -d
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_artisan_prod migrate --force
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_artisan_prod cache:clear
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_artisan_prod config:clear
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_artisan_prod config:cache
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_composer_prod install
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_composer_prod dumpautoload
-docker-compose -f docker-compose-prod.yml run --rm api_sifion_artisan_prod l5-swagger:generate
+docker-compose -f docker-compose-prod.yml build --pull --no-cache
+docker-compose -f docker-compose-prod.yml up -d
+docker-compose -f docker-compose-prod.yml run --rm composer install
+docker-compose -f docker-compose-prod.yml run --rm composer dumpautoload
+docker-compose -f docker-compose-prod.yml run --rm artisan migrate
+docker-compose -f docker-compose-prod.yml run --rm artisan cache:clear
+docker-compose -f docker-compose-prod.yml run --rm artisan config:clear
+docker-compose -f docker-compose-prod.yml run --rm artisan config:cache
+docker-compose -f docker-compose-prod.yml run --rm artisan l5-swagger:generate
+docker-compose -f docker-compose-prod.yml run --rm artisan storage:link
+docker exec -i api_sifion_nginx_prod sh -c "chmod -R 775 /var/www/html/public"
 
 echo "application deployed PROD"
